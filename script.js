7b6a36b93327aa004b8152bfc9d8bfe281af6431
Ugnė Dambrauskė <u.dambrauske@gmail.com>
2023-07-25 09:47:57 +0300

    second-commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('lal')
+const elevator = document.querySelector('.elevator-cont')
+const human = document.querySelector('.human')
+
+const buttonToFloor1 = document.getElementById('button-to-1-floor')
+const buttonToFloor2 = document.getElementById('button-to-2-floor')
+const buttonToFloor3 = document.getElementById('button-to-3-floor')
+
+
+
+const humanAppearsAtRandomFloor = () => {
+    const randomNum = Math.floor(Math.random() * 3) + 1
+    console.log(randomNum)
+
+    human.classList.add(`human-at-${randomNum}-floor`)
+}
+
+humanAppearsAtRandomFloor()
