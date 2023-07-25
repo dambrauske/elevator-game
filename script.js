75e9661c507f3c99e9479a72d22e73284aaeb510
Ugnė Dambrauskė <u.dambrauske@gmail.com>
2023-07-25 10:04:56 +0300

    third-commit

--- script.js
+++ script.js
@@ -5,13 +5,37 @@ const buttonToFloor1 = document.getElementById('button-to-1-floor')
 const buttonToFloor2 = document.getElementById('button-to-2-floor')
 const buttonToFloor3 = document.getElementById('button-to-3-floor')
 
-
+let humanIsAtFloor
+const elevatorComesToHuman = () => {
+    elevator.classList.add(`elevator-at-${humanIsAtFloor}-floor`)
+}
 
 const humanAppearsAtRandomFloor = () => {
     const randomNum = Math.floor(Math.random() * 3) + 1
-    console.log(randomNum)
 
     human.classList.add(`human-at-${randomNum}-floor`)
+    human.classList.add('human-near-elevator')
+    humanIsAtFloor = randomNum
+
+    buttonToFloor1.addEventListener('click', elevatorComesToHuman)
+    buttonToFloor2.addEventListener('click', elevatorComesToHuman)
+    buttonToFloor3.addEventListener('click', elevatorComesToHuman)
 }
 
 humanAppearsAtRandomFloor()
+
+const humanGoesToElevator = () => {
+    human.classList.remove('human-near-elevator')
+    human.classList.add('human-in-elevator')
+}
+
+buttonToFloor1.onclick = () => {
+    elevatorComesToHuman()
+    humanGoesToElevator()
+
+    elevator.classList.remove(`elevator-at-${humanIsAtFloor}-floor`)
+    elevator.classList.add('elevator-at-1-floor')
+}
+
+
+
