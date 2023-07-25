ade21394c2937d63043df2d45a387ebabc0012e9
Ugnė Dambrauskė <u.dambrauske@gmail.com>
2023-07-25 11:22:09 +0300

    fifth commit

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ const buttonToFloor2 = document.getElementById('button-to-2-floor')
 const buttonToFloor3 = document.getElementById('button-to-3-floor')
 
 let humanIsAtFloor
+let currentElevatorFloor = 1
+let targetFloor
+
+
+const humanGoesToElevator = () => {
+    human.style.transform = 'translateX(350%)'
+    setTimeout(() => {
+        human.style.zIndex = '-1'
+    }, 250)
+}
+
+const humanLeavesElevator = () => {
+    setTimeout(() => {
+        human.style.zIndex = '1'
+        human.style.transform = 'translateX(10%)'
+    }, 250)
+
+    human.classList.remove(`human-at-${humanIsAtFloor}-floor`)
+    human.classList.add(`human-at-${targetFloor}-floor`)
+}
+
 const elevatorComesToHuman = () => {
     elevator.classList.remove('elevator-at-1-floor')
     elevator.classList.remove('elevator-at-2-floor')
@@ -29,10 +50,8 @@ const humanAppearsAtRandomFloor = () => {
     floorToGo.classList.add(`target-at-${humanIsAtFloor}-floor`)
 
     generateTargetFloor(1,2,humanIsAtFloor)
+    targetFloor = generateTargetFloor(1,2,humanIsAtFloor)
 
-    // buttonToFloor1.addEventListener('click', elevatorComesToHuman)
-    // buttonToFloor2.addEventListener('click', elevatorComesToHuman)
-    // buttonToFloor3.addEventListener('click', elevatorComesToHuman)
 }
 
 const generateTargetFloor = (min, max, excludedNum) => {
@@ -42,6 +61,10 @@ const generateTargetFloor = (min, max, excludedNum) => {
         randomNum++;
     }
 
+    if (humanIsAtFloor === 1 && randomNum === 1) {
+        randomNum = Math.random() * (3 - 2) + 2
+    }
+
     return floorToGo.textContent = randomNum.toString();
 }
 
@@ -49,10 +72,7 @@ humanAppearsAtRandomFloor()
 
 console.log(humanIsAtFloor)
 
-const humanGoesToElevator = () => {
-    human.classList.remove('human-near-elevator')
-    human.classList.add('human-in-elevator')
-}
+
 
 buttonToFloor1.onclick = () => {
     elevatorComesToHuman()
@@ -60,29 +80,39 @@ buttonToFloor1.onclick = () => {
     setTimeout(() => {
         humanGoesToElevator()
         goalFloorDisappear()
-    }, 1000)
+    }, 250)
 
     setTimeout(() => {
         elevator.classList.remove(`elevator-at-${humanIsAtFloor}-floor`)
         elevator.classList.add('elevator-at-1-floor')
+        currentElevatorFloor = 1
+        setTimeout(() => {
+            humanLeavesElevator()
+        }, 1000)
     }, 1000)
+
+
 
 }
 
 buttonToFloor2.onclick = () => {
     elevatorComesToHuman()
 
-    setTimeout( () => {
+    setTimeout(() => {
         humanGoesToElevator()
         goalFloorDisappear()
-
-    }, 1000)
+    }, 250)
 
 
     setTimeout(() => {
         elevator.classList.remove(`elevator-at-${humanIsAtFloor}-floor`)
         elevator.classList.add('elevator-at-2-floor')
+        currentElevatorFloor = 2
+        setTimeout(() => {
+            humanLeavesElevator()
+        }, 1000)
     }, 1000)
+
 
 }
 
@@ -92,13 +122,44 @@ buttonToFloor3.onclick = () => {
     setTimeout(() => {
         humanGoesToElevator()
         goalFloorDisappear()
-    }, 1000)
+    }, 250)
 
     setTimeout(() => {
         elevator.classList.remove(`elevator-at-${humanIsAtFloor}-floor`)
         elevator.classList.add('elevator-at-3-floor')
+        currentElevatorFloor = 3
+        setTimeout(() => {
+            humanLeavesElevator()
+        }, 1000)
     }, 1000)
 
+
+}
+
+const goToFloor = () => {
+    if (currentElevatorFloor === 1 && targetFloor === 2) {
+        elevator.style.transform = 'translateY(-100%)'
+    }
+
+    if (currentElevatorFloor === 1 && targetFloor === 3) {
+        elevator.style.transform = 'translateY(-200%)'
+    }
+
+    if (currentElevatorFloor === 2 && targetFloor === 1) {
+        elevator.style.transform = 'translateY(100%)'
+    }
+
+    if (currentElevatorFloor === 2 && targetFloor === 3) {
+        elevator.style.transform = 'translateY(200%)'
+    }
+
+    if (currentElevatorFloor === 3 && targetFloor === 1) {
+        elevator.style.transform = 'translateY(200%)'
+    }
+
+    if (currentElevatorFloor === 3 && targetFloor === 2) {
+        elevator.style.transform = 'translateY(100%)'
+    }
 }
 
 
